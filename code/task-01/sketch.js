783480ffbcc461b4377bf40b45446acb0c9566f7
benni-the-w <benni.unbekannt@gmail.com>
2022-11-08 22:43:06 +0100

    Hausaufgabe#1

--- code/task-01/sketch.js
+++ code/task-01/sketch.js
@@ -7,15 +7,21 @@ function setup() {
 }
 
 function draw() {
-  background("#ba55d3");
+  background("#800080");
 
-  fill("#e0ffff")
+  fill("#ff4500")
   rect(200, 0, 200, 400);
 
-  fill("#e0ffff")
+  fill("#ff4500")
   circle(200, 200, 300);
 
-  fill("#ba55d3")
+  fill("#808080")
+  circle(0, 0, 200);
+
+  fill("#ff4500")
+  circle(0, 0, 100);
+
+  fill("#800080")
   angleMode(DEGREES);
 
   arc(
@@ -26,26 +32,37 @@ function draw() {
     PIE 
   );
 
-  fill("#778899 ");
+  fill("#808080");
   arc(
     200, 200,
     200, 200,
     90,
     270,
-    PIE 
+    PIE
   );
 
-  fill("#ee82ee");
-  
-
+  fill("#ff4500");
   arc(
     200, 200,
     100, 100,
     270,
     90,
+     PIE
+  );
+
+  fill("#000000");
+  arc(
+    200, 200,
+    50, 50,
+    90,
+    270,
     PIE 
   );
 
-  fill("#778899 ");
+  fill("rgba(128,128,128,1)");
   rect(250, 0, 50, 400);
+
+  
+
+  
 }
