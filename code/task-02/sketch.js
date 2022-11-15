58b3340b0a0f822c37e1f62e8160de0fc7a2381a
benni-the-w <benni.unbekannt@gmail.com>
2022-11-15 01:56:14 +0100

    hausaufgabe_2

--- code/task-02/sketch.js
+++ code/task-02/sketch.js
@@ -1,6 +1,7 @@
 const circleSize = 20;
 let Farbe_1 = ("#800080")
 let Farbe_2 = ("#ff8c00")
+let Farbe_3 = ("#808080")
 
 
 
@@ -13,14 +14,15 @@ function setup() {
 }
 
 function draw() {
-  background (100);
-  fill(255);
+  background (0);
+  fill(Farbe_3);
   stroke(Farbe_1)
   strokeWeight(2)
+  rectMode(CENTER)
 
-  let x =1
+  let x =0
   while (x <= 300) {
-    let y =1
+    let y =0
     while(y <=300) {
     circle (30 + x, 20 + y, circleSize);
     y += 80;
@@ -32,15 +34,34 @@ function draw() {
     }
   }
 
-  let a =1
+  stroke(Farbe_2)
+  let a =0
   while(a <= 300) {
-    let b =1
+    let b =0
     while(b <=300) {
     circle (0 + a, 60 + b, circleSize);
     b +=80;
     }
     a += 60;
+
+    if (a>=120) {
+      stroke(Farbe_1);
+    }
   }
 
-  
+  fill(Farbe_1)
+  stroke(Farbe_2)
+  let m =0;
+  while(m<= 300) {
+  rect(75 + m, 400, 50, 50);
+  m += 50;
+
+  if(m%20=== 0) {
+    fill(Farbe_1);
+  } 
+   else {
+    fill (Farbe_3);
+  }
+  }
+
 }
